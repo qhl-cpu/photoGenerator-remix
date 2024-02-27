6b35eeb934ce6fd1c52d147c1d8aa00a84c25c78
Davis Qi <907225883wxl@gmail.com>
2024-02-27 12:30:14 -0800

    refactor style for quote cards

--- app/components/QuoteCard.tsx
+++ app/components/QuoteCard.tsx
@@ -10,12 +10,12 @@ export const QuoteCard = ({
 }: QuoteCardPropTypes) => {  
   return (
     <div className="max-w-sm h-[65%] rounded overflow-hidden shadow-lg flex flex-col items-center bg-white bg-opacity-30 border-[15px] border-transparent">
-      <img className="w-250px pt-2" src={quotesPicture}/>
-      <div className="px-2 py-4 font-bold text-white text-xl mb-2">{quotes}</div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+      <img className="w-[280px] pt-2 h-[60%]" src={quotesPicture}/>
+      <div className="px-2 my-4 h-[20%] font-bold text-white text-xl">{quotes}</div>
+      <div className="px-6 pt-4 h-[20%]">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ">#photography</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ">#travel</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ">#winter</span>
       </div>
     </div>
   );
